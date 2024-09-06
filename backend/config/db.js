e860edc23f4440c687435a6e864a3649a5640d43
Logesh PV <139363362+Logeshpv2001@users.noreply.github.com>
2024-09-06 14:32:40 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://logeshkarthi782:logeshpv2001@cluster1.pkyc5ip.mongodb.net/food-del"
+      "mongodb+srv://logeshkarthi782:Logeshpv2001@cluster1.pkyc5ip.mongodb.net/food-del"
     )
     .then(() => console.log("DB Connected"));
 };
